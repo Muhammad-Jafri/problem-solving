2558397bd24c17dfa1d348df205fba877012e6bf
muhammad-jafri <24100113@lums.edu.pk>
2024-12-31 21:57:44 +0000

    take inspiration from lc-1

---
+++ lc-150/lc-219.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        hashmap = {}  # nums[i] + nums[j] = target
+        # nums[j] = target - nums[i]
+
+        for i in range(len(nums)):
+
+            complement = nums[i]
+            if complement in hashmap.keys():
+                j = hashmap[complement]
+                if abs(i - j) <= k:
+                    return True
+
+            hashmap[nums[i]] = i
+
+        return False
+
+
+
+nums = [1,2,3,1,2,3]
+k = 2
+sol = Solution()
+print(sol.containsNearbyDuplicate(nums, k))
