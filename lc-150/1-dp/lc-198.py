0e8ca873e75cbc3fb69d6e1ecad953fb460f6622
muhammad-jafri <24100113@lums.edu.pk>
2025-01-11 01:26:37 +0500

    house robbery

---
+++ lc-150/1-dp/lc-198.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        dp = [0] * (n + 3)
+
+        for i in range(n - 1, -1, -1):
+
+            dp[i] = nums[i] + max(dp[i + 2], dp[i + 3])
+
+        return max(dp[0], dp[1])
+    
+
+nums = [2,7,9,3,1]
+print(Solution().rob(nums=nums))
+
+
