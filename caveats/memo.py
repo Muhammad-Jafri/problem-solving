4c03d8b40aba306450f4891269baf7e16393b6fa
muhammad-jafri <24100113@lums.edu.pk>
2025-01-12 23:39:02 +0500

    memo fib

---
+++ caveats/memo.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+def fib(n: int):
+
+    cache = defaultdict(int)
+
+    def fib_wrapper(n: int):
+
+        if n == 1 or n == 2:
+
+            return 1
+
+        if cache[n]:
+
+            return cache[n]
+
+        cache[n] = fib_wrapper(n - 1) + fib_wrapper(n - 2)
+        return cache[n]
+
+    return fib_wrapper(n)
+
+
+
+print(fib(5))
